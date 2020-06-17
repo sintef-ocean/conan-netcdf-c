6bf60c0a9f221559dfd31dd914ce7df29937ea05
Jarle Ladstein <jarle.ladstein@sintef.no>
2020-06-17 15:17:21 +0200

    Install ++

--- conanfile.py
+++ conanfile.py
@@ -10,17 +10,14 @@ class NetcdfConan(ConanFile):
     description = "<Description of Netcdf here>"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
     generators = "cmake"
     source_subfolder = "netcdf-c"
+    exports = ["patches/*"]
 
     _cmake = None
 
     def requirements(self):
         self.requires("hdf5/1.8.21@sintef/stable")
-
-# Add build requirements: pkgconfig?
 
     def source(self):
         self.run("git clone --no-checkout https://github.com/Unidata/netcdf-c.git {0}".format(self.source_subfolder))
@@ -32,9 +29,6 @@ class NetcdfConan(ConanFile):
         conan_basic_setup()''')
         for patch in ["patches/CMakeLists.txt.patch", "patches/liblib_CMakeLists.txt.patch"]:
             tools.patch(patch_file = patch, base_path = self.source_subfolder, strip=1)
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
 
     def _configure_cmake(self):
         if self._cmake:
@@ -46,12 +40,12 @@ class NetcdfConan(ConanFile):
         self._cmake.definitions["ENABLE_TESTS"] = False
         self._cmake.definitions["BUILD_SHARED_LIBS"] = False
         self._cmake.definitions["ENABLE_DYNAMIC_LOADING"] = True
+        self._cmake.definitions["BUILD_SHARED_LIBS"] = False
         #self._cmake.definitions[""] = 
         self._cmake.configure(source_folder="{0}".format(self.source_subfolder))
         return self._cmake
 
     def build(self):
-        print("{0}".format(self.source_subfolder))
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -61,7 +55,8 @@ class NetcdfConan(ConanFile):
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        print("test package")
+        cmake = self._configure_cmake()
+        cmake.install()
         #self.copy("*.h", dst="include", src="hello")
         #self.copy("*hello.lib", dst="lib", keep_path=False)
         #self.copy("*.dll", dst="bin", keep_path=False)
@@ -70,8 +65,7 @@ class NetcdfConan(ConanFile):
         #self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["netcdf-c"]
-        #self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["netcdf"]
 
     def configure(self):
         del self.settings.compiler.libcxx
